49576c488a76ffb6e4d362edb3dc3795e9253f82
sgnl <rayrfarias@gmail.com>
2015-07-07 10:03:57 -1000

    update code documentation

--- smallest_multiple.js
+++ smallest_multiple.js
@@ -1,11 +1,14 @@
-/*  smallestMultiple(multiplicands, digits)
+/**
+ * Build a function that finds the smallest positive number that is evenly
+ * divisible by all of the numbers starting from 1 up to the value passed into your function.
  *
- *  @param numbers        the amount of numbers to multiply by
- *
- *  @returns the smallest positive number that is evenly divisible by all numbers
+ * @param  { Number } ceiling This value will serve as your ceiling.
+ * @return { Number }         Lowest Positive Number that is evenly divisible by all numbers
+ *                            between 1 and `ceiling`
  */
 module.exports = function( ceiling ) {
   // do work here
+
 
   return 0;
 };
