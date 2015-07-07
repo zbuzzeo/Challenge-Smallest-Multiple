7a308757b8dc345f7544b7ee83cc677388b029f1
sgnl <rayrfarias@gmail.com>
2015-07-07 10:20:35 -1000

    update spec

--- test/smallest_multiple-spec.js
+++ test/smallest_multiple-spec.js
@@ -10,6 +10,7 @@ describe('Smallest number', function(){
   });
 
   it('that can be divided by each of the numbers from 1 to 20 without any remainder', function(){
+    this.timeout(0); // so our test won't timeout. 0 disables timeout function in Mocha.
     var result = smallestMultiple(20);
     expect(result).to.equal(232792560);
   });
